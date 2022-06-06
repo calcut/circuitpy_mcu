ac5d3772270072d14e3ee8f3c27c4c401dc02d5d
Calum Cuthill <calum.cuthill@gmail.com>
2022-06-06 14:44:27 +0100

    dual bank sort of working

--- cp_ota.py
+++ cp_ota.py
@@ -1,24 +1,43 @@
-from lib.circuitpy_mcu.mcu import Mcu
 import adafruit_requests
 import ssl
 import socketpool
 import wifi
 import dualbank
+from secrets import secrets
+import microcontroller
 
-
-mcu = Mcu()
-mcu.wifi_connect()
+print('connecting to wifi')
+wifi.radio.connect(secrets["ssid"], secrets["password"])
+print('wifi connected')
 
 pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
+# see https://github.com/adafruit/circuitpython/issues/6448
 
-url = f'https://downloads.circuitpython.org/bin/adafruit_feather_esp32s2/en_GB/adafruit-circuitpython-adafruit_feather_esp32s2-en_GB-7.2.5.bin'
-response = requests.get(url)
+url = 'https://adafruit-circuit-python.s3.amazonaws.com/bin/adafruit_feather_esp32s2/en_GB/adafruit-circuitpython-adafruit_feather_esp32s2-en_GB-7.3.0.bin'
+# url= 'https://adafruit-circuit-python.s3.amazonaws.com/bin/adafruit_feather_esp32s2/en_GB/adafruit-circuitpython-adafruit_feather_esp32s2-en_GB-7.2.5.bin'
 
-chunk_size = 1024
-offset=0
-for chunk in response.iter_content(chunk_size):
-    print(f'offset = {offset}')
-    dualbank.flash(chunk, offset=offset)
-    offset += chunk_size
+offset=65536
+position = 0
+header = {}
+header["Range"] = f"bytes={offset}-"
+response = requests.get(url, headers=header)
+length = int(response.headers["content-length"])
+
+for chunk in response.iter_content(chunk_size=8192):
+    actual_chunk_size = len(chunk)
+    print(f'{position=} {round(position/length*100, 1)}%')
+    try:
+        dualbank.flash(chunk, offset=position)
+        position += actual_chunk_size
+    except RuntimeError as e:
+        print(f'RuntimeError: {e}')
+        print('switching OTA banks and resetting')
+        dualbank.switch()
+        microcontroller.reset()
+        
+
+print('flash complete')
+dualbank.switch()
+microcontroller.reset()
